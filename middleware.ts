d5bf8b146c269be56dfd31cc64bed781fa1f1824
anubhav-aryan <aryananubhav08@gmail.com>
2024-03-19 15:57:53 +0530

    fix: auth fix

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ export default authMiddleware({
   //  publicRoutes: ['/'],
   // Prevent the specified routes from accessing
   // authentication information:
-  // ignoredRoutes: ['/no-auth-in-this-route'],
+  ignoredRoutes: ['/'],
 });
  
 export const config = {
